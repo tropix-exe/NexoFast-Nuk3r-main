cd373ad8573379ae93136bfb0533e56fbb86a39c
Tropix <leondodo268@gmail.com>
2025-04-17 13:29:42 +0200

    updated main and added first cmd

--- NexoFast.py
+++ NexoFast.py
@@ -4,21 +4,22 @@ import asyncio
 import os
 
 
-TOKEN = "MTMyNTU0ODAyNTY0NDMxODg1Mg.G7-Toi.UtZVrdcYZ2OfefuwJj-Z4_2Uutsqyza24moQWI"
-PREFIX = "&"
+TOKEN = ""  # Your bot token here (idk who is going to use this but just in case)
+PREFIX = "&" # Your bot prefix here you can use any prefix you want
+
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=PREFIX, intents=intents, help_command=None)
 
 @bot.event
 async def on_ready():
-    print(f"✅  Logged in as {bot.user} (ID: {bot.user.id})")
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
 
 async def load_extensions():
     for file in os.listdir("./commands"):
         if file.endswith(".py") and not file.startswith("_"):
             await bot.load_extension(f"commands.{file[:-3]}")
-            print(f"🔌  Loaded commands.{file[:-3]}")
+
 
 async def main():
     async with bot:
